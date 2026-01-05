0e2d5263a261141733e820920b61ed4849f393db
Tittuss <149028025+Tittuss@users.noreply.github.com>
2026-01-05 18:13:19 -0400

    Creacion y configuracion de DTOs, Mapping y UseCases

---
+++ Application/DTOs/UpdateGradeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class UpdateGradeDto
+    {
+        public Guid CourseId { get; set; }
+        public Guid StudentId { get; set; }
+        public decimal NewGrade { get; set; }
+    }
+}
---
+++ Application/UseCases/Teachers/ITeacherService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Teachers
+{
+    public interface ITeacherService
+    {
+        Task<IEnumerable<CourseSimpleDto>> GetAssignedCoursesAsync(Guid teacherId);
+        Task<IEnumerable<StudentGradeDto>> GetStudentsByCourseAsync(Guid courseId);
+        Task UpdateStudentGradeAsync(UpdateGradeDto dto);
+    }
+}
