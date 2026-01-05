469df74ea59200a84b5365d79218861447794e03
Tittuss <149028025+Tittuss@users.noreply.github.com>
2026-01-05 19:11:37 -0400

    Controllers y configuracion del Program en WebApi

---
+++ WebApi/Controllers/TeacherController.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs;
+using Application.UseCases.Teachers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherService _teacherService;
+
+        public TeacherController(ITeacherService teacherService)
+        {
+            _teacherService = teacherService;
+        }
+
+        [HttpGet("{teacherId}/courses")]
+        public async Task<IActionResult> GetAssignedCourses(Guid teacherId)
+        {
+            var courses = await _teacherService.GetAssignedCoursesAsync(teacherId);
+            return Ok(courses);
+        }
+
+        [HttpGet("course/{courseId}/students")]
+        public async Task<IActionResult> GetStudentsByCourse(Guid courseId)
+        {
+            var students = await _teacherService.GetStudentsByCourseAsync(courseId);
+            return Ok(students);
+        }
+
+        [HttpPut("grade")]
+        public async Task<IActionResult> UpdateGrade([FromBody] UpdateGradeDto gradeDto)
+        {
+            try
+            {
+                await _teacherService.UpdateStudentGradeAsync(gradeDto);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
